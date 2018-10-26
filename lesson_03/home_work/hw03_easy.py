183e46df832595944ad552b3e1647e950cc4c934
ton-and-polo <ton.and.polo@gmail.com>
2018-10-27 00:20:56 +0300

    done 2-nd part hw03_easy

--- lesson_03/home_work/hw03_easy.py
+++ lesson_03/home_work/hw03_easy.py
@@ -4,6 +4,9 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+# 1:
+print("1st part:")
+
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -11,6 +14,27 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
+# 2:
+print("2nd part:")
+
+def lucky_ticket(number):
+    number = str(number)
+    result = ""
+    def sum(lst):
+        result = 0
+        for i in lst:
+            result += int(i)
+        return result
+    left_part_sum = sum(number[:3])
+    right_part_sum = sum(number[3:])
+    if left_part_sum == right_part_sum:
+        result = True
+    else:
+        result = False
+    return result
+
+print(lucky_ticket(590185)) # True
+print(lucky_ticket(998001)) # False
 
 
 
