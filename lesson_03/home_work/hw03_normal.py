744874f3b73d64be3b1434efedf9fe6983aa3707
ton-and-polo <ton.and.polo@gmail.com>
2018-10-28 16:13:47 +0300

    done 3rd part hw03_normal

--- lesson_03/home_work/hw03_normal.py
+++ lesson_03/home_work/hw03_normal.py
@@ -26,14 +26,14 @@ print(fibonacci_sequence(4, 10))
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 # 2:
-print("3rd part:")
+print("2rd part:")
 
 my_list = [-10, 2, 66, 7, 3, 0, 66]
 
 def bubble_sort(lst):
     counter = 1
     while counter < len(lst):
-         for i in range(len(lst) - counter): # - counter: not compare sorted i
+         for i in range(len(lst) - counter):
               if lst[i] > lst[i+1]:
                   lst[i], lst[i+1] = lst[i+1], lst[i]
          counter += 1
@@ -44,6 +44,32 @@ print(bubble_sort(my_list))
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+
+# 3:
+print("3rd part:")
+
+# first my_filter() argument (function):
+def my_func(x):
+  if x < 18:
+    return False
+  else:
+    return True
+
+# second my_filter() argument (iterable):
+ages = [5, 12, 17, 18, 24, 32]
+
+def my_filter(function, iterable):
+    result =[]
+    for i in iterable:
+        if function(i) == True:
+            result.append(i)
+
+    return result
+
+
+adults = my_filter(my_func, ages)
+print(adults)
+
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
