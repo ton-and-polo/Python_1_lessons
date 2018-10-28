90024a82cefcadb528bc4494df87f7c03a0969eb
ton-and-polo <ton.and.polo@gmail.com>
2018-10-28 17:27:57 +0300

    done 1st part hw03_easy

--- lesson_03/home_work/hw03_easy.py
+++ lesson_03/home_work/hw03_easy.py
@@ -7,6 +7,14 @@
 # 1:
 print("1st part:")
 
+def my_round(number, digits):
+    number = number * (10 ** digits) + 0.5
+    number = number // 1
+    return number / (10 ** digits)
+
+print(my_round(0.6, 0))
+print(my_round(0.4, 0))
+
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -19,23 +27,15 @@ print("2nd part:")
 
 def lucky_ticket(number):
     number = str(number)
-    result = ""
     def sum(lst):
         result = 0
         for i in lst:
             result += int(i)
         return result
-    left_part_sum = sum(number[:3])
-    right_part_sum = sum(number[3:])
-    if left_part_sum == right_part_sum:
-        result = True
+    if sum(number[:3]) == sum(number[3:]): # compare left and rigth sum
+        return True
     else:
-        result = False
-    return result
+        return False
 
 print(lucky_ticket(590185)) # True
 print(lucky_ticket(998001)) # False
-
-
-
-
